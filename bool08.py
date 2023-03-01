aee358a2ab62b16c5eac809b1db561096ae89721
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-01 23:27:28 +0500

    x8

--- bool08.py
+++ bool08.py
@@ -1,13 +1,12 @@
 def main(a):
     """
     check the whole number. Integers are 0 and a positive number.
-    Butun raqamni tekshiring. Butun sonlar 0 va musbat sondir.
     Args:
         a: int
     Returns:
         bool
     """
     # Write your code here
-    return a >= 0
+    return a == int(a) >= 0
 
-print(main(-1))
+print(main(19))
