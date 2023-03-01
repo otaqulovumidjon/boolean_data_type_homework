826128475e6d0351867775ffe8a810f229a7f531
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-01 20:00:38 +0500

    10

--- bool10.py
+++ bool10.py
@@ -1,13 +1,13 @@
-from math import sqrt
 def main(a):
     """
     Check that the number "a" is a perfect square.
+    “a” soni mukammal kvadrat ekanligini tekshiring.
     Args:
         a: int
     Returns:
         bool
     """
     # Write your code here
-    return sqrt(a)
+    return a == (a ** (1/2)) ** 2
 
-print(main(9))
+print(main(121))
